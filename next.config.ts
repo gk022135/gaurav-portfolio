7dad03876ec2f1238abe44e0d952ae9b35ef0f6e
gk022135 <gk022135@gmail.com>
2025-05-14 16:10:21 +0530

    elslint  diabled

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['assets.aceternity.com','images.app.goo.gl','images.unsplash.com'], // Add the external image domain here
   },
+  eslint: {
+    ignoreDuringBuilds: true, // <- THIS
+  },
 };
 
 
