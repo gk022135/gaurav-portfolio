8112bc89f7412c04731d065a44fdd11dd9e89de6
gk022135 <gk022135@gmail.com>
2026-01-22 00:38:42 +0530

    build fix done

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     domains: ['assets.aceternity.com', 'images.app.goo.gl', 'images.unsplash.com'],
   },
